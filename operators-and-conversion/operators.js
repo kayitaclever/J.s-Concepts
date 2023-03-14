38192dafbf3deed9aef5204cf2d08c00ecb194c8
kayitaclever <kayitaramirwaaclever@gmail.com>
2023-03-14 11:37:37 +0300

    first commit

--- operators-and-conversion/operators.js
+++ operators-and-conversion/operators.js
@@ -6,15 +6,15 @@ console.log(num-num2)
 console.log(num/num2)
 console.log(num%num2)
 console.log(num*num2)
-
+  
 console.log(num>30);
 
 const a = 10;
 const b ="10";
 console.log("loosely equal", a == b);
 console.log("loosely not equal", a !=b);
-console.log("strictly equal", a==b);
-console.log("strictly not equal", a !=b);
+console.log("strictly equal", a===b);
+console.log("strictly not equal", a !==b);
 if(a !== b && num === num2){
     console.log(true)
 }
